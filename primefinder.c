56c4d46dc839f1f037b5403127094b7e560c8f3d
Vincent De Comarmond <vincent.de.comarmond.sa@gmail.com>
2024-05-01 21:30:07 +0200

    Moved computation function to elsewhere

--- primefinder.c
+++ primefinder.c
@@ -1,48 +1,48 @@
 #include <stdio.h>
 #include <stdbool.h>
-int main()
-{
 
-    int current_prime=2, desired_primes;
-    long current_value=3, previous_value=2;
+void generatePrimes(int desiredPrimes, long primeNumbers[]) {
 
-    // Ask user how many primes they want
-    printf("Enter desired number of primes: ");
-    //Store the range in variables using scanf
-    scanf("%d", &desired_primes);
-    printf("Desired primes: %d:\n", desired_primes);
+    // Give this the first 2 primes to bootstrap
+    int currentPrime=2;
+    long currentValue = 3l;
+    primeNumbers[0]=2, primeNumbers[1]=currentValue;
 
-    // Array to store primes in
-    long primes[desired_primes];
-    primes[0] = 2, primes[1] = 3;
+    while (currentPrime < desiredPrimes) {
+        currentValue++;
 
-    //Display prime numbers for input range
-    printf("Generating first %d prime numbers:\n", desired_primes);
-
-    while (current_prime < desired_primes) {
-        previous_value = current_value;
-        current_value += 1;
-
-        // printf("Current value: %ld, Current prime: %d\n", current_value, current_prime); // Debugging
+        // printf("Current value: %ld, Current prime: %d\n", currentValue, currentPrime); // Debugging
         bool isprime = true;
-        for (int i = 0 ; i < current_prime; i ++) {
+        for (int i = 0 ; i < currentPrime; i ++) {
             // printf("Testing prime: %ld\n", primes[i]); // Debugging
-            if (current_value % primes[i] == 0) {
+            if (currentValue % primeNumbers[i] == 0) {
                 isprime = false;
                 break;
             }
         }
         if (isprime) {
-            primes[current_prime] = current_value;
-            current_prime +=1;
+            primeNumbers[currentPrime] = currentValue;
+            currentPrime++;
         }
     }
+}
 
 
-    for (int i = 0; i < current_prime; i ++) {
-        printf("%ld\n",primes[i]);
+int main()
+{
+
+    int desiredPrimes;
+    // ask user how many primes they want
+    printf("enter desired number of primes: ");
+    //store the range in variables using scanf
+    scanf("%d", &desiredPrimes);
+    printf("desired primes: %d:\n", desiredPrimes);
+    long primes[desiredPrimes];
+    generatePrimes(desiredPrimes, primes);
+
+    for (int i = 0; i < desiredPrimes; i ++) {
+        printf("%ld\n", primes[i]);
     }
-
 
     return 0;
 }
